1f9baa7becf26ddb1d00e27382896bf4bdc84f83
Daniel Warner <contact@dfos.ca>
2020-08-17 15:34:07 -0600

    fixed code indentation

--- footer.php
+++ footer.php
@@ -15,9 +15,13 @@
 			<div class="row">
 				<div class="col-md-4">
 					<?php dynamic_sidebar( 'footer-1' ); ?>
-				</div>				<div class="col-md-4">
+				</div>
+				
+				<div class="col-md-4">
 					<?php dynamic_sidebar( 'footer-2' ); ?>
-				</div>				<div class="col-md-4">
+				
+				</div>
+				<div class="col-md-4">
 					<?php dynamic_sidebar( 'footer-3' ); ?>
 				</div>
 			</div>
